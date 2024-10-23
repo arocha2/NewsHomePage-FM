f15b797e68949ca89ae415ba0124573f6fc70f05
arocha2 <abda08.ab@gmail.com>
2024-10-22 21:15:21 -0400

    add: grid hero started

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,24 @@
+import imgDesktop from "../assets/images/image-web-3-desktop.jpg";
+import imgMobile from "../assets/images/image-web-3-mobile.jpg";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-3 px-6">
+      <div className=" md:col-span-2 border border-red-600">
+        <img
+          src={imgDesktop}
+          alt="img-desktop"
+          className="hidden sm:block w-full"
+        />
+        <img
+          src={imgMobile}
+          alt="img-desktop"
+          className="block w-full sm:hidden"
+        />
+      </div>
+      <div className="border md:row-span-2 row-start-4 border-red-600">2</div>
+      <div className="border border-red-600">3</div>
+      <div className="border border-red-600">4</div>
+    </section>
+  );
 };
